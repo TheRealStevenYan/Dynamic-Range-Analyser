ec81fb0c1d6a2ea4d6eda03a56a93fdd6e2bc5cc
Steven <steven.yan7888@gmail.com>
2021-03-05 22:12:20 -0800

    more gui work
    took the first step towards implementing persistence

--- operations/ui_operations.py
+++ operations/ui_operations.py
@@ -1,14 +1,18 @@
 import tkinter as tk
+
+from pip._vendor.colorama import Fore, Style
 from operations.file_operations import AudioData
 
 
 class MainGUI:
-
     def __init__(self):
         self.window = tk.Tk()
+        self.window.geometry("800x600")
         self.audio_data = AudioData()
         self.__audio_data_record = []
+        self.output = tk.Text(self.window)
 
+        self.__update_window()
         self.make_open_file_button()
         self.window.mainloop()
 
@@ -24,24 +28,33 @@ class MainGUI:
             self.__audio_data_record.append(self.audio_data)
             print(self.__audio_data_record)
         self.audio_data.open_file()
-        self.__print_data()
+        self.__update_window()
 
-    # Will probably delete later. Prints things.
-    def __print_data(self):
-        print(interpret_dr(self.audio_data.dynamic_range))
-        print(interpret_score(self.audio_data.dynamic_range_score))
+    # Updates the text on the window. Bunch of tkinter stuff going on here.
+    def __update_window(self):
+        self.output.configure(state="normal")
+        self.output.delete('1.0', tk.END)
+        self.output.tag_configure("center", justify='center')
+        self.__print_scores()
+        self.output.tag_add("center", "1.0", "end")
+        self.output.configure(state="disabled")
+        self.output.pack()
+
+    def __print_scores(self):
+        self.output.insert(tk.INSERT, interpret_dr(self.audio_data.dynamic_range))
+        self.output.insert(tk.INSERT, interpret_score(self.audio_data.dynamic_range_score))
 
 
 # Returns a string which interprets the dynamic range it is given.
 def interpret_dr(dynamic_range):
     return_string = "The dynamic range of this file is {dr} dB.\n" \
-                    "This is {quality}.".format(dr=dynamic_range, quality=__dr_assessment(dynamic_range))
+                    "This is {quality}.\n".format(dr=dynamic_range, quality=__dr_assessment(dynamic_range))
     return return_string
 
 
 def interpret_score(score):
     return_string = "The dynamic range score of this file is {s}. \n" \
-                    "This file has {quality} dynamic range.".format(s=score, quality=__score_assessment(score))
+                    "This file has {quality} dynamic range.\n".format(s=score, quality=__score_assessment(score))
     return return_string
 
 
@@ -59,6 +72,7 @@ def __score_assessment(score):
 
 
 # Returns a string which describes the quality of the audio file based on its dynamic range.
+# Coloured text described here. https://ozzmaker.com/add-colour-to-text-in-python/
 def __dr_assessment(dynamic_range):
     if 100 <= dynamic_range <= 144:
         return "Hi-Resolution Audio (24-bit)"
@@ -66,3 +80,5 @@ def __dr_assessment(dynamic_range):
         return "CD Quality (16-bit)"
     elif 60 <= dynamic_range < 80:
         return "Vinyl LP Quality"
+    else:
+        return "Unknown"
